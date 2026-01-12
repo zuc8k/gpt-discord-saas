5a3d01124cd8ab9721d34382b06c2e3757b22eea
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-12 14:20:54 +0200

    Create plans.js

---
+++ shared/plans.js
@@ -0,0 +1,34 @@
+module.exports = {
+  FREE: {
+    days: 7,
+    monthlyLines: 10000,
+    yearlyLines: 20000
+  },
+
+  PRIME: {
+    price: {
+      monthly: 5,
+      yearly: 13
+    },
+    monthlyLines: 10000,
+    yearlyLines: 20000
+  },
+
+  PREMIUM: {
+    price: {
+      monthly: 10,
+      yearly: 25
+    },
+    monthlyLines: 20000,
+    yearlyLines: 40000
+  },
+
+  MAX: {
+    price: {
+      monthly: 20,
+      yearly: 50
+    },
+    monthlyLines: 40000,
+    yearlyLines: 80000
+  }
+};
