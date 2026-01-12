3eccc9e5d370fe9985f923092937b037d83e2c88
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-12 14:21:13 +0200

    Create Guild.js

---
+++ api/models/Guild.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const guildSchema = new mongoose.Schema({
+  guildId: { type: String, unique: true },
+  plan: { type: String, default: "FREE" },
+
+  expiresAt: Date,
+
+  monthlyLimit: Number,
+  yearlyLimit: Number,
+
+  usedLines: { type: Number, default: 0 },
+
+  lastReset: Date,
+
+  logsChannel: String,
+  gptChannel: String
+}, { timestamps: true });
+
+module.exports = mongoose.model("Guild", guildSchema);
