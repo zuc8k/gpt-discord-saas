2bafa7f71e76d7b616043cb29a62568008138e05
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-12 15:40:29 +0200

    Create commandLimits.js

---
+++ shared/commandLimits.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ask: {
+    FREE: 5,
+    PRIME: 20,
+    PREMIUM: 50,
+    MAX: Infinity
+  },
+
+  upgrade: {
+    FREE: 0,
+    PRIME: 0,
+    PREMIUM: 0,
+    MAX: 10
+  }
+};
