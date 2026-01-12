13f443c45428a8144787d364d02a91b2018dea35
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-12 14:28:14 +0200

    Create interactionCreate.js

---
+++ bot/events/interactionCreate.js
@@ -0,0 +1,13 @@
+module.exports = async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    interaction.reply({ content: "❌ حصل خطأ", ephemeral: true });
+  }
+};
